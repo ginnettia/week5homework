225e6c62498fab3ae8804a093298d76b20cd70db
ginnettia <adamhginnetti@gmail.com>
2021-03-08 21:28:12 -0500

    changes

--- fun/homework.py
+++ fun/homework.py
@@ -7,6 +7,7 @@ def find_greatest_number(incoming_list):
     Find the largest number in the list.
     """
     return max(incoming_list)
+    pass
 
 
 def find_least_number(incoming_list):
@@ -15,6 +16,7 @@ def find_least_number(incoming_list):
     Find the smallest/least number in the list.
     """
     return min(incoming_list)
+    pass
 
 
 def add_list_numbers(incoming_list):
@@ -23,6 +25,7 @@ def add_list_numbers(incoming_list):
     Add all the values together and return it.
     """
     return sum(incoming_list)
+    pass
 
 
 def longest_value_key(incoming_dict):
@@ -31,3 +34,4 @@ def longest_value_key(incoming_dict):
     Find the KEY that has a value with the highest length, use the len() function
     """
     return max(incoming_dict.keys(), key=len)
+    pass
