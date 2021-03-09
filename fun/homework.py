b17e6e9670d64b08accb5b70f4081d3547f469d6
ginnettia <adamhginnetti@gmail.com>
2021-03-08 22:24:24 -0500

    changes

--- fun/homework.py
+++ fun/homework.py
@@ -1,26 +1,17 @@
 """Homework file for my students to have fun with some algorithms! """
 
 
-def find_greatest_number(incoming_list):
-    
+def find_greatest_number(incoming_list):  
     return max(incoming_list)
     pass
-
-
-def find_least_number(incoming_list):
-    
+def find_least_number(incoming_list):  
     return min(incoming_list)
     pass
-
-
 def add_list_numbers(incoming_list):
     
     return sum(incoming_list)
     pass
-
-
-def longest_value_key(incoming_dict):
-    
+def longest_value_key(incoming_dict): 
     if incoming_dict is not None:
         if len(incoming_dict) == 0:
             return None
